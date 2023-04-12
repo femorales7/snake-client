d75d92ad8ccc2c4616b5e599328d2a1b0d9bfed6
Your Name <your@email.com>
2023-04-12 14:35:15 -0600

    inplemented client file and export the funtions

---
+++ client.js
@@ -0,0 +1,26 @@
+// clien.js
+const net = require("net");
+const connect = function () {
+  const conn = net.createConnection({
+    host: "172.31.217.78",// IP address here,
+    port: 50541// PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on("connect", () => {
+
+    console.log(`his is the mesage of server`);
+    // code that does something when the connection is first established
+  });
+  conn.on('data', (data) => {
+    console.log(`message from the server`, data );
+  } )
+  
+ 
+  return conn;
+};
+module.exports = {
+  connect
+}
+
