eb38c40d70767b9006e53e038f45465fdd2cf958
Your Name <your@email.com>
2023-04-12 15:42:36 -0600

    set the functions setipInput and handleUserInput

--- play.js
+++ play.js
@@ -4,4 +4,20 @@ const {connect} = require("./client")
 console.log("Connecting ...");
 connect();
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
 
+stdin.on("data", handleUserInput);
+
+const handleUserInput = function () {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+setupInput();
