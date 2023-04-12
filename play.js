9932ee5bb1932058711f1aae0b7da9b0ce2ccc38
Your Name <your@email.com>
2023-04-12 15:53:04 -0600

    implemented input  and call functions

--- play.js
+++ play.js
@@ -1,23 +1,8 @@
 
 const net = require("net");
 const {connect} = require("./client")
+const {setupInput} = require("./input");
 console.log("Connecting ...");
 connect();
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  return stdin;
-};
-
-stdin.on("data", handleUserInput);
-
-const handleUserInput = function () {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
 
 setupInput();
